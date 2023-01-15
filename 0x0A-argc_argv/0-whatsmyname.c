61878fe67a356eec50ef730c19d7881f1d45ecb9
profgbest <profgbest@gmail.com>
2023-01-15 08:34:04 -0800

    Final commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,17 @@
+#include "holberton.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints file name
- * @argc: first parameter
- * @argv: second parameter
- * Description: a program that prints its name
- * Return: Always(0) Success
- */
-
-int main(int argc, char **argv)
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
 {
-	while (argc--)
-	{
-		printf("%s\n", *argv++);
-	}
+	(void) argc;
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
