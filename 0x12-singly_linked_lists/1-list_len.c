cfa87e2b679db905713d95deb24df8e0897ac30a
profgbest <profgbest@gmail.com>
2023-02-05 14:36:19 -0800

    Singly linked

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,29 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in h
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
  */
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
- size_t n = 0;
+	int count = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
-}
-
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+} /*Author: Godwin*/
